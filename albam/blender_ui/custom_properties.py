9ba6ef89f9b16997da60d2a6e2bf8f9c4a0fde49
HenryOfCarim <imhenryviii@gmail.com>
2024-03-10 17:25:38 +0200

    Initial implementation of param copypaster
    Store to json part is commented for now because it doesn't work as it should for Blender's vector properties

--- albam/blender_ui/custom_properties.py
+++ albam/blender_ui/custom_properties.py
@@ -11,9 +11,9 @@ class CopyPasterBuff(bpy.types.PropertyGroup):
 
 
 @blender_registry.register_blender_type
-class CopyCustomPropertiesMat(bpy.types.Operator):
+class CopyCustomPropsMat(bpy.types.Operator):
     """Copy Albam material properties from the active material"""
-    bl_idname = "material.custom_property_copy"
+    bl_idname = "material.custom_props_copy"
     bl_label = "Copy Properties"
 
     @classmethod
@@ -24,14 +24,13 @@ class CopyCustomPropertiesMat(bpy.types.Operator):
         mat = bpy.context.active_object.active_material.name
         if mat:
             bpy.context.scene.albam_copypaster.mat_buff = mat
-            print(mat)
         return {'FINISHED'}
 
 
 @blender_registry.register_blender_type
-class PasteCustomPropertiesMat(bpy.types.Operator):
+class PasteCustomPropsMat(bpy.types.Operator):
     """Paste Albam material properties to the active material"""
-    bl_idname = "material.custom_property_paste"
+    bl_idname = "material.custom_props_paste"
     bl_label = "Paste Properties"
 
     @classmethod
@@ -77,9 +76,9 @@ class PasteCustomPropertiesMat(bpy.types.Operator):
 
 
 @blender_registry.register_blender_type
-class StoreCustomPropertiesMat(bpy.types.Operator):
+class StoreCustomPropsMat(bpy.types.Operator):
     """Copy Albam material properties from the active material"""
-    bl_idname = "material.custom_property_store"
+    bl_idname = "material.custom_props_store"
     bl_label = "Store to a file"
 
     filepath: bpy.props.StringProperty(subtype="DIR_PATH")
@@ -100,6 +99,7 @@ class StoreCustomPropertiesMat(bpy.types.Operator):
                 params[attr_name] = getattr(active_mat.albam_custom_properties.mod_156_material, attr_name)
             else:
                 params[attr_name] = getattr(active_mat.albam_custom_properties.mrl_params, attr_name)
+
         with open(self.filepath, "w") as file:
             file.write(json.dumps(params, indent=4))
             print("store mat params")
@@ -125,9 +125,10 @@ class StoreCustomPropertiesMat(bpy.types.Operator):
         return albam_asset
 
 
-class LoadCustomPropertiesMat(bpy.types.Operator):
+@blender_registry.register_blender_type
+class LoadCustomPropsMat(bpy.types.Operator):
     """Load Albam material properties from a file to the active material"""
-    bl_idname = "material.custom_property_load"
+    bl_idname = "material.custom_props_load"
     bl_label = "Load from a file"
 
     filepath: bpy.props.StringProperty(subtype="DIR_PATH")
@@ -152,6 +153,59 @@ class LoadCustomPropertiesMat(bpy.types.Operator):
 
 
 @blender_registry.register_blender_type
+class CopyCustomPropsMesh(bpy.types.Operator):
+    """Copy Albam mesh properties from the active mesh"""
+    bl_idname = "mesh.custom_props_copy"
+    bl_label = "Copy Properties"
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def execute(self, context):
+        obj = bpy.context.active_object.name
+        if obj:
+            bpy.context.scene.albam_copypaster.mesh_buff = obj
+            # print(bpy.types.Scene.albam_copypaste_buffer_mesh)
+        return {'FINISHED'}
+
+
+@blender_registry.register_blender_type
+class PasteCustomPropsMesh(bpy.types.Operator):
+    """Paste Albam mesh properties to the active mesh"""
+    bl_idname = "mesh.custom_props_paste"
+    bl_label = "Paste Properties"
+
+    @classmethod
+    def poll(cls, context):
+        if not bpy.context.scene.albam_copypaster.mesh_buff:
+            return False
+        return True
+
+    def execute(self, context):
+        copied_obj_name = bpy.context.scene.albam_copypaster.mesh_buff
+        try:
+            copied_mesh = bpy.data.objects.get(copied_obj_name)
+        except:
+            bpy.context.scene.albam_copypaster.mesh_buff = ""
+        active_bl_obj = bpy.context.active_object
+        parent = active_bl_obj.parent
+        app_id = parent.albam_asset.app_id
+        custom_props = active_bl_obj.data.albam_custom_properties.get_appid_custom_properties(
+            app_id)
+        for attr_name in custom_props.__annotations__:
+            if app_id == "re5":
+                setattr(active_bl_obj.data.albam_custom_properties.mod_156_mesh,
+                        attr_name,
+                        getattr(copied_mesh.data.albam_custom_properties.mod_156_mesh, attr_name))
+            else:
+                setattr(active_bl_obj.data.albam_custom_properties.mod_21_mesh,
+                        attr_name,
+                        getattr(copied_mesh.data.albam_custom_properties.mod_21_mesh, attr_name))
+        return {'FINISHED'}
+
+
+@blender_registry.register_blender_type
 class ALBAM_PT_CustomPropertiesMaterial(bpy.types.Panel):
     bl_idname = "ALBAM_PT_CustomPropertiesMaterial"
     bl_label = "Custom Properties (Albam)"
@@ -165,15 +219,15 @@ class ALBAM_PT_CustomPropertiesMaterial(bpy.types.Panel):
 
     def draw(self, context):
         mat = context.material
-        layout = self.layout  # add layout for Albam material panel
+        layout = self.layout
         row = layout.row()
-        row.operator("material.custom_property_copy")
-        row.operator("material.custom_property_store",
-                     icon="SORT_ASC", text="")
+        row.operator("material.custom_props_copy")
+        # row.operator("material.custom_props_store",
+        #             icon="SORT_ASC", text="")
         row = layout.row()
-        row.operator("material.custom_property_paste")
-        row.operator("material.custom_property_load",
-                     icon="SORT_DESC", text="")
+        row.operator("material.custom_props_paste")
+        # row.operator("material.custom_props_load",
+        #             icon="SORT_DESC", text="")
         albam_asset = self.albam_asset_uses_mat(mat)  # already called in poll, can we save this?
         app_id = albam_asset.app_id
         custom_props = mat.albam_custom_properties.get_appid_custom_properties(app_id)
@@ -209,6 +263,11 @@ class ALBAM_PT_CustomPropertiesMesh(bpy.types.Panel):
 
     def draw(self, context):
         albam_asset = self.albam_asset_uses_mesh(context.mesh)  # already called in poll, can we save this?
+        layout = self.layout
+        row = layout.row()
+        row.operator("mesh.custom_props_copy")
+        row = layout.row()
+        row.operator("mesh.custom_props_paste")
         app_id = albam_asset.app_id
         custom_props = context.mesh.albam_custom_properties.get_appid_custom_properties(app_id)
         for k in custom_props.__annotations__:
